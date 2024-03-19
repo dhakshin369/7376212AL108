24346561af1072f85f992a43d5e312b37accc61a
dhakshin369 <96608712+dhakshin369@users.noreply.github.com>
2024-03-19 16:16:41 +0530

    committed all source codes at once

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http" //responsible for basic http server
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	http.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "dhakshinraj")
+	})
+	http.HandleFunc("/regnumber", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "7376212AL108")
+	})
+	http.HandleFunc("/department", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "AIML")
+	})
+	http.HandleFunc("/color", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "YELLOW")
+	})
+
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/hello\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
